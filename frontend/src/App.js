c82c69c5d38e99e96b8cfbb4659e231d59fd9127
Animesh <108383866+Animesh1771@users.noreply.github.com>
2023-06-24 08:51:14 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import AddNotes from "./page/addnote";
+import { Home } from "./page/home";
+import { Login } from "./page/login";
+// import { NotesPage } from "./page/Note";
+import Signup from "./page/signup";
+import { Todo } from "./page/todo";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UpdateNotes from "./page/update";
+import NotesPage from "./page/Note";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/Login" element={<Login />}></Route>
+            <Route exact path="/signup" element={<Signup />}></Route>
+            <Route exact path="/Home" element={<Home />}></Route>
+            <Route exact path="/Updatenote" element={<UpdateNotes />}></Route>
+            <Route exact path="/Addnote" element={<AddNotes />}></Route>
+          </Routes>
+          {!localStorage.getItem("userid") ? (
+            console.log(1)
+          ) : (
+            <Routes>
+              <Route exact path="/Todo" element={<Todo />}></Route>
+              <Route exact path="/Notepage" element={<NotesPage/>}></Route>
+            </Routes>
+          )}
+        </div>
+      </Router>
     </div>
   );
 }
